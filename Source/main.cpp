12664543dcebe3071ccae481327e6ab45a75f2a7
Galaxysplash <143564354+Galaxysplash@users.noreply.github.com>
2025-03-27 17:55:02 +0100

    ...

---
+++ Source/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main(int argc, char **argv) {
+    if (argc > 2) {
+        std::cout << "Too many args\n";
+    } else if (argc == 2) {
+        //runFile(argc[1]);
+    } else {
+        //runPrompt();
+    }
+}
